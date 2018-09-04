8891ab1a967d35bc013e6eddaf2b6f110d5de767
xian7758 <jose731@sina.com>
2018-09-04 09:37:27 +0800

    2018/9/4

--- src/controller/Backendlogin.java
+++ src/controller/Backendlogin.java
@@ -1,11 +1,5 @@
 package controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.sun.istack.internal.logging.Logger;
 
 @Controller
-public class IndexController {
+public class Backendlogin {
 	private Logger logger = Logger.getLogger(IndexController.class);
-	
-	@RequestMapping("/dev/login")
+	@Autowired
+	@RequestMapping("/manager/login")
 	public String Login(){
-			logger.info("开发者平进来了！");
-		return "devlogin";
+			logger.info("管理者平台进来了！");
+		return "backendlogin";
 	}
-	
-	
-
 }
