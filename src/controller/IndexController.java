7af0dd431994ac99ec8d5ff66c6eb986ce2d5a45
xian7758 <jose731@sina.com>
2018-09-04 08:47:47 +0800

    web.xml

---
+++ src/controller/IndexController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sun.istack.internal.logging.Logger;
+
+@Controller
+public class IndexController {
+	
+	private Logger logger = Logger.getLogger(IndexController.class);
+	
+	@Autowired
+	@RequestMapping("/manager/login")
+	public String Login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+			logger.info("½øÀ´ÁË£¡");
+		return "backendlogin";
+	}
+	
+	
+
+}
---
+++ src/dao/UserDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+import java.util.Map;
+
+public interface UserDao {
+		public int DoLogin(Map<String,Object> userMap);
+}
